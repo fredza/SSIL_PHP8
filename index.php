28ac48b65a9630ff6b8efcab754a81c9afc44c99
fredza <fredza801@hotmail.com>
2022-02-14 15:05:01 +0100

    Tour d'horizon des types de données basiques

--- index.php
+++ index.php
@@ -1,6 +1,32 @@
 <?php
-// define('NAME', 'Fred');
-const NAME = 'Fred';
+$name = 'Fred';
+$double = "Elle a dit : \t \"Hello $name\"\n";
+////////////////////////////////////////////////////////
+$age = 25;
+$result = 22.;
+$hex = 0xfff;
+$bin = 0b11101;
+$population = 6_658_925_456;
+$isAdmin = true;
+$names = [
+    'Domi',
+    'Fred',
+    'Laure',
+    'Florian',
+    52,
+    true,
+];
+$associatif = [
+    'name' => 'Fred',
+    'age' => 52,
+    'mail' => 'fredza@localhost',
+    'valnul' => null,
+];
 
-echo NAME;
+//echo $double;
+//echo $population;
+echo $names[0];
+echo $associatif['mail'];
+echo $associatif['valnul'];
+var_dump($associatif['valnul']);
 
