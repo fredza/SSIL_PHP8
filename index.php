a4b7268a5e8640da5f8037c5e8ee982eb851b0ef
fredza <fredza801@hotmail.com>
2022-02-15 12:17:54 +0100

    Les opérateurs arithméthiques et les fonctions mathémathiques

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
-
-$name = 'Fred';
-//$salutation = 'Hello '.$name.PHP_EOL;
-//$salutation = "Hello $name".PHP_EOL;
-//$salutation = sprintf('Salut %s%s', $name, PHP_EOL);
-// printf fait un echo
-printf('Salut %s%s', $name, PHP_EOL);
-
-
-//echo $salutation;
+$add = 5 + 32.5 + 16;
+$sub = $add - 12;
+$multiply = $sub * 2;
+$divide = $multiply / 3;
+$rest = 21 % 2;
+$rest2 = 10 % 2;
+var_dump($add, $sub, $multiply, $divide, $rest, $rest2);
