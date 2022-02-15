3cf31654ef6ec16c4834dc10cfa7ec46bbbe1ea8
Frédéric Zawalski <fredza801@gmail.com>
2022-02-15 15:18:38 +0100

    Les opérateurs logiques

--- index.php
+++ index.php
@@ -1,31 +1,9 @@
 <?php
 require __DIR__.'/vendor/autoload.php';
 
-$nb1 = 2.0;
-$nb2 = 2;
-$nb3 = 3;
+$age = 20;
 
-dump(
-    $nb1 == $nb2,
-    $nb1 === $nb2,
-    $nb1 != $nb2,
-    $nb1 !== $nb2,
-    'supp et inf',
-    $nb1 > $nb3,
-    $nb1 >=$nb3,
-    $nb1 < $nb3,
-    $nb1 <= $nb3,
-);
-//----------------------------Les Booléens----------------------------
-dump('Les Booléens',
-(""),        // bool(false)
-("0"),       // bool(false)
-(1),         // bool(true)
-(-2),        // bool(true)
-("foo"),     // bool(true)
-(2.3e5),     // bool(true)
-(array(12)), // bool(true)
-(array()),   // bool(false)
-("false"),   // bool(true)
-[] == false,
-);
+//dump($age > 18 && $age != 25);
+$size = 180;
+dump($age >= 18 || $size >= 160);
+dump(!($age >= 18 || $size >= 160));
