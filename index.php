185b6f31f87903d579f7197d438379cd0984645e
Frédéric Zawalski <fredza801@gmail.com>
2022-02-15 15:26:53 +0100

    L'opérateur Null Coalescing

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 require __DIR__.'/vendor/autoload.php';
 
-$age = 20;
-
-//dump($age > 18 && $age != 25);
-$size = 180;
-dump($age >= 18 || $size >= 160);
-dump(!($age >= 18 || $size >= 160));
+$people = ['name' => 'Fred'];
+$users = $members ?? $people ?? [];
+dump($users);
