e17baf3077e68bdae5fa5421c937aea10878cf75
fredza <fredza801@hotmail.com>
2022-02-15 12:25:16 +0100

    Bonus avoir un meilleur vardumper (nous utiliserons le vardumper de Symfony et utiliserons l'autoload.

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
+require __DIR__.'/vendor/autoload.php';
+
 $add = 5 + 32.5 + 16;
 $sub = $add - 12;
 $multiply = $sub * 2;
 $divide = $multiply / 3;
 $rest = 21 % 2;
 $rest2 = 10 % 2;
-var_dump($add, $sub, $multiply, $divide, $rest, $rest2);
+dd($add, $sub, $multiply, $divide, $rest, $rest2);
