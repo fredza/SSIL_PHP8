edc9c95fcf4d1900e924dce2b8ded296faf200ca
Frédéric Zawalski <fredza801@gmail.com>
2022-02-16 12:33:57 +0100

    Un raccourci n'est jamais de trop

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
 require __DIR__.'/vendor/autoload.php';
 
-$people = ['name' => 'Fred'];
-$users = $members ?? $people ?? [];
-dump($users);
+$number = 28;
+//$number = $number *2;
+$number *= 2;
+$name = "Frédéric".PHP_EOL;
+$message = 'Bonjour ';
+$message .= $name;
+//$user = $user ?? [];
+$user = 'FRED';
+$user ??= 'Frédéric';
+dump($number, $message, $user);
+printf('Le nom %s et le numéro est %s', $user, $number);
+
