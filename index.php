b867f894d924665ad6a835fcfbce014910f09739
Frédéric Zawalski <fredza801@gmail.com>
2022-02-16 18:03:51 +0100

    La boucle for

--- index.php
+++ index.php
@@ -1,25 +1,9 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-$posts = [
-    [],
-    [],
-];
-
-//if ($posts) {
-//    echo 'Il ya des résultats'.PHP_EOL;
-//} else {
-//    echo 'Il n\'y a pas de résultats'.PHP_EOL;
-//}
-$result = $posts ? 'Il y a des résultats' : 'Il n\'y a pas de résultats';
-echo($result.PHP_EOL);
-if (count($posts) >= 2) {
-    // Instructions si vraies
-    printf('Il y a %s articles%s', count($posts), PHP_EOL);
-} elseif (count($posts) === 1) {
-    echo 'Il y a 1 article' . PHP_EOL;
-
-} else {
-    echo 'Il n\'y a aucun article'.PHP_EOL;
+for ($i=1; $i <= 100; $i++) {
+    echo 'boucle '.$i.PHP_EOL;
 }
+
+
 
