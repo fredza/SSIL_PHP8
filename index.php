66a3a875402918ed995cce71a92f57f1cb3d5590
Frédéric Zawalski <fredza801@gmail.com>
2022-02-16 15:06:47 +0100

    La structure if

--- index.php
+++ index.php
@@ -1,9 +1,24 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-$expr1 = false;
-$expr2 = true;
-$expr3 = false;
+$posts = [
+    [],
+    [],
+];
 
-dump(($expr1 == $expr2) && ($expr2 || $expr3));
+if (!$posts) {
+    echo 'Il n\'y a pas de résultats'.PHP_EOL;
+} else {
+    echo 'Il ya des résultats'.PHP_EOL;
+}
+
+if (count($posts) >= 2) {
+    // Instructions si vraies
+    printf('Il y a %s articles%s', count($posts), PHP_EOL);
+} elseif (count($posts) === 1) {
+    echo 'Il y a 1 article' . PHP_EOL;
+
+} else {
+    echo 'Il n\'y a aucun article'.PHP_EOL;
+}
 
